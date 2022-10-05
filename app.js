d2faae08fc5569e28775e6df26c6c0803c27be1a
sumura-alpcom <113319132+sumura-alpcom@users.noreply.github.com>
2022-10-05 16:46:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   console.log("Get request is called");
-  res.send("Hello World!");
+  res.send("Hello World 3!");
 });
 
 app.listen(port, () => {
